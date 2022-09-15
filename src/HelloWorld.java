28f840c224ca3014e3603e0db7d0152e8e53e9f4
joozepp <podelsky@gmail.com>
2022-09-15 21:31:31 +0300

    please work v3

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * The first hello world program
+ *
+ * @author Joozepp
+ */
+public class HelloWorld {
+    public static void main(String[] args) { // shortcut is psvm
+        System.out.println("Hello, World!");
+    }
+}
