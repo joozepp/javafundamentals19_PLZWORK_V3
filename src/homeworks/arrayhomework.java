73e81bef6d7c037a6c8e166d5d9436af08f7c660
joozepp <podelsky@gmail.com>
2022-09-27 18:07:53 +0300

    arrayhomework

--- src/homeworks/arrayhomework.java
+++ src/homeworks/arrayhomework.java
@@ -3,7 +3,7 @@ package homeworks;
 public class arrayhomework {
     public static void main(String[] args) {
         int[] x = {1,7,3,10,9};
-        int result = x[0];  //b[0] = 9
+        int result = x[0];
 
         for(int z: x) {
             if(z < result) {
